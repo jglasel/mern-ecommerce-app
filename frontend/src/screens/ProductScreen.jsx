2654b37259554f859787820d6172dea50f51e3f3
kusiub <kusiub@gmail.com>
2022-06-02 15:10:09 +0000

    added productscreen

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Row , Col , Image, ListGroup, Card, Button} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+
+const ProductScreen = () => {
+
+
+  return (
+    <div>ProductScreen</div>
+  )
+}
+
+export default ProductScreen
