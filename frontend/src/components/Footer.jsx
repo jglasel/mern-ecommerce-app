18a0845c62d915dfeac72717556e3dea4d0fe805
kusiub <kusiub@gmail.com>
2022-06-02 11:09:15 +0000

    added products and rating components

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container , Row , Col} from 'react-bootstrap';
+
+const Footer = () => {
+  
+  const date = new Date().getFullYear();
+  
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+          Copyright &copy; kusiub {date}
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
